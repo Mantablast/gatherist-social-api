b61c242b6e0e870a55a80e8da45dd8b484880d13
AimeeJay <ajesso@post.com>
2021-06-22 20:44:53 -0400

    fixed reactions schema to fit user criteria

--- models/Thought.js
+++ models/Thought.js
@@ -2,44 +2,77 @@ const { Schema, model } = require('mongoose');
 
 const ThoughtSchema = new Schema(
     {
-    thoughtText: {
-        type: String,
-      unique: true,
-      required: 'Text is required',
-      min: [1, 'Must be between 1 and 280 characters'],
-      max: 280
+        thoughtText: {
+            type: String,
+            unique: true,
+            required: 'Text is required',
+            min: [1, 'Must be between 1 and 280 characters'],
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //reseach what is meant by createdAtVal (created at validation)
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ]
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        //reseach what is meant by createdAtVal (created at validation)
-        get: createdAtVal => dateFormat(createdAtVal)
-      },
-    username: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'User'
-        }
-      ],
-    reactions: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Reaction'
-        }
-    ]
-},
     {
         toJSON: {
-          virtuals: true,
-          getters: true
+            virtuals: true,
+            getters: true
         },
         //prevent duplicates
         id: false
     }
 )
-
+//reaction schema placed in though js file as asked
+//subdocument schema
+const reactionSchema = new Schema({
+    reactionId: {
+        //mongoose object id
+        type: Schema.Types.ObjectId,
+        //Default value is set to a new ObjectId
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        required: true,
+        type: string,
+        max: 280
+        //no min specified
+    },
+    username: {
+        required: true,
+        type: string
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //reseach what is meant by createdAtVal (created at validation) from pizza hunt lesson
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        //prevent duplicates
+        id: false
+    }
+);
 const Thought = model('Thought', ThoughtSchema);
 
 //Create a virtual called friendCount that retrieves the len of the user's friends array field on query.
-
 module.exports = Thought;
--- server.js
+++ server.js
@@ -11,32 +11,33 @@ const { User } = require('./models');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/virtualsdb', {
+//startup mongoose virtual
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gatheristdb', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
+//seeding
 mongoose.set('useCreateIndex', true);
 mongoose.set('debug', true);
 
-app.post('/submit', ({ body }, res) => {
-  const user = new User(body);
+// app.post('/submit', ({ body }, res) => {
+//   const user = new User(body);
 
-  User.create(user)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+//   User.create(user)
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-app.get('/users', (req, res) => {
-  User.find({}).then(users => {
-    res.json(users);
-  });
-});
+// app.get('/users', (req, res) => {
+//   User.find({}).then(users => {
+//     res.json(users);
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
